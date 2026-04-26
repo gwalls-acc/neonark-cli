9964a666a99b54a4cd49f277d14f8299fc405b4f
Gloria Walls <gloria.walls@g.austincc.edu>
2026-04-25 21:23:43 -0500

    initial commit

---
+++ neonark-cli/neonark-cli/src/main/java/org/example/neonarkcli/NeonarkCliApplication.java
@@ -0,0 +1,13 @@
+package org.example.neonarkcli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeonarkCliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeonarkCliApplication.class, args);
+    }
+
+}
