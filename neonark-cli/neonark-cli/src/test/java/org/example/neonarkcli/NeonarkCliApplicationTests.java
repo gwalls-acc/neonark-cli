f316ad5b3141e7e1249848c59477c4982881c26c
Gloria Walls <gloria.walls@g.austincc.edu>
2026-05-14 23:11:13 -0500

    submitting project 4

---
+++ neonark-cli/neonark-cli/src/test/java/org/example/neonarkcli/NeonarkCliApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.neonarkcli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeonarkCliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
